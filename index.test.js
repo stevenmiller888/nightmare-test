058b26d40cef57d435b3f7581763d982100d4f4b
Steven Miller <stevenmiller888@me.com>
2016-11-24 10:52:37 -0800

    bunch of random tests

--- index.test.js
+++ index.test.js
@@ -7,3 +7,51 @@ test('can visit url', async t => {
   t.is(page.url, url)
   t.is(page.code, 200)
 })
+
+test('can visit url', async t => {
+  const url = 'https://segment.com/'
+  const page = await Nightmare().goto(url)
+  t.is(page.url, url)
+  t.is(page.code, 200)
+})
+
+test('can visit url', async t => {
+  const url = 'http://abcnews.go.com/'
+  const page = await Nightmare().goto(url)
+  t.is(page.url, url)
+  t.is(page.code, 200)
+})
+
+test('can visit url', async t => {
+  const url = 'http://www.cnn.com/'
+  const page = await Nightmare().goto(url)
+  t.is(page.url, url)
+  t.is(page.code, 200)
+})
+
+test('can visit url', async t => {
+  const url = 'https://github.com/'
+  const page = await Nightmare().goto(url)
+  t.is(page.url, url)
+  t.is(page.code, 200)
+})
+
+test('can visit url', async t => {
+  const url = 'https://www.facebook.com/'
+  const page = await Nightmare().goto(url)
+  t.is(page.url, url)
+  t.is(page.code, 200)
+})
+
+test('can visit url', async t => {
+  const url = 'https://www.apple.com/'
+  const page = await Nightmare().goto(url)
+  t.is(page.url, url)
+  t.is(page.code, 200)
+})
+
+test('can visit url and click a link', async t => {
+  const url = 'https://www.apple.com/'
+  const link = await Nightmare().goto(url).click('a[href="/mac/"]').url()
+  t.is('https://www.apple.com/mac/', link)
+})
